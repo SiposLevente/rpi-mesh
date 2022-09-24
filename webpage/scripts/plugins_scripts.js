0477405298984b96e8d75b73efd9f2346f220a89
SiposLevente <sipi.levente@gmail.com>
2022-09-24 23:41:29 +0200

    modified plugins page

--- webpage/scripts/plugins_scripts.js
+++ webpage/scripts/plugins_scripts.js
@@ -1,1 +1,11 @@
+$.ajax({
+        method: "POST",
+        url: "./scripts/plugins.php",
+        data: {
+            text: $("div.plugin_menu").text()
+        }
+    })
+    .done(function(response) {
 
+        $("div.plugin_menu").html(response);
+    });
