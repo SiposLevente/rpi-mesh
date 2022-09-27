d25b7f74b3c8ac9aea984763e3e020cb6a03f532
SiposLevente <sipi.levente@gmail.com>
2022-09-27 21:20:21 +0200

    added interface removing from mesh

---
+++ scripts/remove_if_from_mesh.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo batctl if del $1
---
+++ webpage/scripts/remove_if_from_mesh.php
@@ -0,0 +1,5 @@
+<?php
+$interface = $_POST["if_from_mesh"];
+shell_exec("/etc/node-scripts/remove_if_from_mesh.sh $interface");
+header('Location: ../configuration.html');
+?>
