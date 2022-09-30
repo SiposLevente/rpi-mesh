d0a1aee1ae25623e355ba999d6df581b47c173fb
SiposLevente <sipi.levente@gmail.com>
2022-09-30 10:47:32 +0200

    added sudo to scripts

--- scripts/add_bridge.sh
+++ scripts/add_bridge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ip link add name $3 type bridge
-ip link set dev $1 master $3
-ip link set dev $2 master $3
-ip link set up dev $1
-ip link set up dev $2
-ip link set up dev $3
+sudo ip link add name $3 type bridge
+sudo ip link set dev $1 master $3
+sudo ip link set dev $2 master $3
+sudo ip link set up dev $1
+sudo ip link set up dev $2
+sudo ip link set up dev $3
--- scripts/delete_forwarding.sh
+++ scripts/delete_forwarding.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -D FORWARD -i $2 -o $1 -j ACCEPT
-iptables -D FORWARD -i $1 -o $2 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -D POSTROUTING -o $1 -j MASQUERADE
+sudo iptables -D FORWARD -i $2 -o $1 -j ACCEPT
+sudo iptables -D FORWARD -i $1 -o $2 -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -t nat -D POSTROUTING -o $1 -j MASQUERADE
