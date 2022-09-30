fe606b5ab6f88f8e1b8a9a31d04feb24e78cb7b3
SiposLevente <sipi.levente@gmail.com>
2022-09-30 10:40:38 +0200

    added forwarding settigns

---
+++ scripts/add_forwarding.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -A FORWARD -i $2 -o $1 -j ACCEPT
+iptables -A FORWARD -i $1 -o $2 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -A POSTROUTING -o $1 -j MASQUERADE
