092d13e62fda56993544dfdb3896c4dce1b5fd95
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-03 12:26:57 +0200

    fixed interface getting scripts

--- webpage/scripts/settings_scripts.js
+++ webpage/scripts/settings_scripts.js
@@ -7,11 +7,11 @@ $.ajax({
         url: "./scripts/interfaceIp.php",
     })
     .done(function(response) {
-        var data_entries = response.replaceAll(/<.?p>|<.?span>/gm, '').split('\n');
+        var data_entries = response.replaceAll(/<.?p>|<.?span>/gm, '').split('\n').filter(function (element) { return element != "" });
 //        var dhcp_selector = document.getElementById("dhcp_selector");
         var ap_selector = document.getElementById("ap_selector");
 
-        for (var i = 0; i < data_entries.length - 1; i++) {
+        for (var i = 0; i < data_entries.length; i++) {
             var split_data = data_entries[i].split('/');
             var interface_plus_ip = split_data[0].split(':');
             if (interface_plus_ip[1].trim() == "Offline") {
