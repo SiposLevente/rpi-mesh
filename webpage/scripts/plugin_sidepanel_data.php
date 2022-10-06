d2de10e68ca501af4edd701ddc1f4692efaf179c
SiposLevente <sipi.levente@gmail.com>
2022-10-06 14:06:40 +0200

    made improvements to plugins

--- webpage/scripts/plugin_sidepanel_data.php
+++ webpage/scripts/plugin_sidepanel_data.php
@@ -4,7 +4,7 @@ $plugin_folder = "../plugins/";
 
 $info = json_decode(file_get_contents($plugin_folder . $plugin_name."/info.json"));
 
-echo "<h3 style=\"text-align: center\">$info->pluginName</h3><center>v$info->version by $info->author</center><p>$info->long_description</p><div class=\"plugin_settings\"><a class=\"settings_button\" href=\"./plugins/$plugin_name/pages/settings.php\" ><button>Settings</button></a></div>"
+echo "<h3 style=\"text-align: center\">$info->pluginName</h3><center>v$info->version by $info->author</center><p>$info->long_description</p><div class=\"plugin_settings\"><a class=\"settings_button\" href=\"./plugins/$plugin_name/pages/settings.html\" ><button>Settings</button></a></div>"
 
 
 
