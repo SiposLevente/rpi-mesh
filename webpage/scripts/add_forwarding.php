c29051b22dd1255df43c2b5b7a88b66628d7e78c
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-07 21:29:58 +0200

    bugfix

--- webpage/scripts/add_forwarding.php
+++ webpage/scripts/add_forwarding.php
@@ -1,6 +1,6 @@
 <?php
 $if1 = $_POST["if1"];
 $if2 = $_POST["if2"];
-shell_exec("sudo /etc/node-scripts/add_forwarding.sh $if1 $if2")
+shell_exec("sudo /etc/node-scripts/add_forwarding.sh $if1 $if2");
 header('Location: ../configuration.html');
 ?>
