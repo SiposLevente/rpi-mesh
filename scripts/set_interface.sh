a2da921fa2a77b1b86cea4a1199408fce35601ee
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-11 00:23:35 +0200

    added newlines

--- scripts/set_interface.sh
+++ scripts/set_interface.sh
@@ -7,12 +7,12 @@ sed -i.bak "/$2/,/^$/{/^$/!d}" $interfaces_file
 ip address flush dev $2
 case $1 in
     "static")
-        echo -en "interface $2\nstatic ip_address=$3/$4\nstatic routers=$5\nstatic domain_name_servers=$6 $7" >> $dhcp_file
+        echo -en "interface $2\nstatic ip_address=$3/$4\nstatic routers=$5\nstatic domain_name_servers=$6 $7\n\n" >> $dhcp_file
         sed -i '/^$/N;/^\n$/D' $dhcp_file 
         systemctl 
         ;;
     "dhcp")
-        echo -en $"\nauto $2\n  iface $2 inet dhcp\n" >> $interfaces_file
+        echo -en $"\nauto $2\n  iface $2 inet dhcp\n\n" >> $interfaces_file
         sed -i '/^$/N;/^\n$/D' $interfaces_file
         ;;
     "avahi")
