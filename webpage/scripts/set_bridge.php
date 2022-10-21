78a4e29d4f25fe4a64ca45480dee47f3ac8acc1a
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-21 13:46:06 +0200

    added missing semicolon

--- webpage/scripts/set_bridge.php
+++ webpage/scripts/set_bridge.php
@@ -2,6 +2,6 @@
 $if1 = $_POST["if1"];
 $if2 = $_POST["if2"];
 $bridge_name = $_POST["bridge_name"];
-shell_exec("sudo /etc/node-scripts/add_bridge.sh $if1 $if2 $bridge_name")
+shell_exec("sudo /etc/node-scripts/add_bridge.sh $if1 $if2 $bridge_name");
 header('Location: ../configuration.html');
 ?>
