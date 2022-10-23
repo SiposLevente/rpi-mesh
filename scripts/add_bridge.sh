148c85586562bc0b5a14b8a44517e093b09786c4
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-23 15:08:15 +0200

    improved bridge saving

--- scripts/add_bridge.sh
+++ scripts/add_bridge.sh
@@ -6,9 +6,11 @@ ip link set up dev $1
 ip link set up dev $2
 ip link set up dev $3
 
+echo -en "\n# $3" >> /etc/node-scripts/start_node.sh
 echo "ip link add name $3 type bridge" >> /etc/node-scripts/start_node.sh
 echo "ip link set dev $1 master $3" >> /etc/node-scripts/start_node.sh
 echo "ip link set dev $2 master $3" >> /etc/node-scripts/start_node.sh
 echo "ip link set up dev $1" >> /etc/node-scripts/start_node.sh
 echo "ip link set up dev $2" >> /etc/node-scripts/start_node.sh
 echo "ip link set up dev $3" >> /etc/node-scripts/start_node.sh
+echo -en "\n" >> /etc/node-scripts/start_node.sh
--- scripts/delete_bridge.sh
+++ scripts/delete_bridge.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
+sed -i "/$1/,/^$/{/^$/!d}" /etc/node-scripts/start_node.sh
+sed -i "/^$/N;/^\n$/D" /etc/node-scripts/start_node.sh
+
 ip link set $1 down
 ip link delete $1
-
-
-sed -i "/ip link add name $1 type bridge/d" /etc/node-scripts/start_node.sh
-sed -i "/ip link set up dev $1/d" /etc/node-scripts/start_node.sh
-
-for interface in /sys/class/net/$1/brif/* ; do
-        if_name=${interface##*/}
-        sed -i "/ip link set dev $if_name master $1/d" /etc/node-scripts/start_node.sh
-        sed -i "/ip link set up dev $if_name/d" /etc/node-scripts/start_node.sh
-done
