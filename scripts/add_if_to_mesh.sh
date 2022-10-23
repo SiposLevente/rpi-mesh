1e17d6fea58a20f9ceea52951e487f9a02542c6c
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-23 21:07:20 +0200

    removing interface ip if adding interface to mesh

--- scripts/add_if_to_mesh.sh
+++ scripts/add_if_to_mesh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 batctl if add $1
+echo "/etc/node-scripts/remove_ip_interface.sh $1" >> /etc/node-scripts/start_node.sh
 echo "batctl if add $1" >> /etc/node-scripts/start_node.sh
--- scripts/remove_if_from_mesh.sh
+++ scripts/remove_if_from_mesh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 batctl if del $1
+sed -i "/\/etc\/node-scripts\/remove_ip_interface.sh $1/d" /etc/node-scripts/start_node.sh
 sed -i "/batctl if add $1/d" /etc/node-scripts/start_node.sh
