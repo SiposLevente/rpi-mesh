5e4cd7408db79fdc3dfc485f9cb42bff4f28126d
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-23 09:49:09 +0200

    added wifi disconnecting

--- scripts/toggle_interface.sh
+++ scripts/toggle_interface.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 status=`ip add show $1 | grep " DOWN "`
 if [[ "$status" == "" ]];then
+    first_arg=$1
+    if [[ ${first_arg::1} == "w" ]];then
+        nmcli d disconnect $1
+    fi
     sudo ip link set $1 down
 else
     sudo ip link set $1 up 
