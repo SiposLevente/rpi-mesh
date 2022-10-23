803d6b39b808f600dedbcb165c174b1fb060403d
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-23 21:32:09 +0200

    fixed bug

--- scripts/add_if_to_mesh.sh
+++ scripts/add_if_to_mesh.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 batctl if add $1
+/etc/node-scripts/remove_ip_interface.sh $1
+
 echo "/etc/node-scripts/remove_ip_interface.sh $1" >> /etc/node-scripts/start_node.sh
 echo "batctl if add $1" >> /etc/node-scripts/start_node.sh
