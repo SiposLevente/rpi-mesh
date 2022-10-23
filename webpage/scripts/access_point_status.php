d6b6c6f3bf5e60cbecc4afa1c2eb183c5293810c
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-23 23:40:44 +0200

    bugfixe

--- webpage/scripts/access_point_status.php
+++ webpage/scripts/access_point_status.php
@@ -1,12 +1,10 @@
 <?php
-$active = shell_exec("systemctl is-active hostapd.service");
-if($active == "active")
-{
-$result = "ONLINE";
+$active = exec("systemctl is-active hostapd.service");
+if($active == "active") {
+    $result = "ONLINE";
 }
-else
-{
-$result =  "OFFLINE";
+else {
+    $result = "OFFLINE";
 }
 echo $result;
 ?>
